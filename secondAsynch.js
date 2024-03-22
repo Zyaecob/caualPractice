f593802ae6b9a875327452c9909678833c5813bb
ZEREAY YAECOB <113598782+Zyaecob@users.noreply.github.com>
2024-03-22 13:28:08 +0300

    new assyn example

---
+++ secondAsynch.js
@@ -0,0 +1,13 @@
+// Script.js
+function f1() {
+  console.log("f1");
+}
+function f2() {
+  console.log("f2");
+}
+function main() {
+  console.log("main");
+  setTimeout(f1, 1000);
+  f2();
+}
+main();
